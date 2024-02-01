3460d899d0e82c6878b35ef3f6ae2c9fdfa74597
ecgit <quochuy030702@gmail.com>
2024-02-01 23:45:17 +0700

    add func

--- train/train_PBAFN_stage1_fs.py
+++ train/train_PBAFN_stage1_fs.py
@@ -68,7 +68,7 @@ optimizer_warp = torch.optim.Adam(params_warp, lr=opt.lr, betas=(opt.beta1, 0.99
 if opt.continue_train and opt.PBAFN_warp_checkpoint:
     checkpoint = torch.load(opt.PBAFN_warp_checkpoint, map_location= {'cuda:0': 'cuda:1'})
     # ckp = refresh(checkpoint['model_state_dict'])
-    model.load_state_dict(checkpoint)
+    model.load_state_dict(checkpoint['model_state_dict'])
     optimizer_warp.load_state_dict(checkpoint['optimizer_state_dict'])
     start_epoch = checkpoint['epoch'] + 1
 
