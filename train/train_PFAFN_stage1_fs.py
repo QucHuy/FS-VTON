e6911791990146c86cec6fbc57437ee675dc1065
ecgit <quochuy030702@gmail.com>
2024-02-22 08:39:50 +0700

    fix

--- train/train_PFAFN_stage1_fs.py
+++ train/train_PFAFN_stage1_fs.py
@@ -96,9 +96,9 @@ optimizer_part = torch.optim.Adam(params_part, lr=opt.lr, betas=(opt.beta1, 0.99
 
 if opt.continue_train and opt.PFAFN_warp_checkpoint_continue:
     warp_checkpoint = torch.load(opt.PFAFN_warp_checkpoint_continue)
-    w_ckp = refresh(warp_checkpoint['model_state_dict'])
+    # w_ckp = refresh(warp_checkpoint['model_state_dict'])
     # load_checkpoint_parallel(PF_warp_model, opt.PFAFN_warp_checkpoint_continue, 1)
-    PF_warp_model.load_state_dict(w_ckp)
+    PF_warp_model.load_state_dict(warp_checkpoint['model_state_dict'])
     optimizer.load_state_dict(warp_checkpoint['optimizer_state_dict'])
     optimizer_part.load_state_dict(warp_checkpoint['optimizer_part_state_dict'])
     start_epoch = warp_checkpoint['epoch'] + 1
